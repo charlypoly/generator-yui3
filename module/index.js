8f30181614ce801cbea32a2c8bdf11251701961b
Charly POLY <cpoly55@gmail.com>
2013-08-14 16:01:04 +0200

    improve module generator with assets, lang and templates

--- module/index.js
+++ module/index.js
@@ -13,6 +13,19 @@ var ModuleGenerator = module.exports = function ModuleGenerator(args, options, c
 
 util.inherits(ModuleGenerator, yeoman.generators.NamedBase);
 
+ModuleGenerator.prototype.configure = function() {
+    this.configuration = {};
+
+    this.configuration.assets = !!(this.options['assets'] && !this.options['!assets']);
+
+    this.configuration.i18n = !!(this.options['i18n'] && !this.options['!i18n']);
+    
+    this.configuration.tests = !!(this.options['tests'] && !this.options['!tests']);
+    
+    this.configuration.templates = !!(this.options['templates'] && !this.options['!templates']);
+
+    console.log('configuration : ', this.configuration);
+}
 
 /**
  * @method beforeCreate
@@ -33,7 +46,7 @@ ModuleGenerator.prototype.createStructure = function createStructure() {
     this.mkdir(this.name);
     this.mkdir(this.name + "/js");
     this.template("_moduleName.js", this.name + "/js/" + this.name + ".js");
-    this.template("_build.json", this.name + "/build.json");
+    this.write(this.name +"/build.json", this._generateBuild());
 };
 
 /**
@@ -42,8 +55,8 @@ ModuleGenerator.prototype.createStructure = function createStructure() {
  */
 ModuleGenerator.prototype.meta = function meta() {
     this.mkdir(this.name + "/meta");
-    this.template("meta/_meta.json", this.name + "/meta/" + this.name + ".json");
-}
+    this.write(this.name +"/meta/" + this.name + ".json", this._generateMeta());
+};
 
 /**
  * @method tests
@@ -63,7 +76,22 @@ ModuleGenerator.prototype.tests = function tests() {
         process.exit(1);
     }
 
-}
+};
+
+
+ModuleGenerator.prototype.templates = function templates() {
+    if (this.configuration.templates) {
+        this.mkdir(this.name + "/templates/");
+        this.write(this.name + "/templates/"+this.name+".handlebars.html", "");
+    }
+};
+
+ModuleGenerator.prototype.assets = function assets() {
+    if (this.configuration.assets) {
+        this.mkdir(this.name + "/assets/");
+        // this.write(this.name + "assets/"+this.name+".handlebars.html", "");
+    }
+};
 
 // private
 // -------------------------------------------------------------------------------------------------
@@ -75,7 +103,7 @@ ModuleGenerator.prototype.tests = function tests() {
 ModuleGenerator.prototype._getProjectName = function _getProjectName() {
     var pkg = JSON.parse(this.readFileAsString(path.join(process.cwd(), '../package.json')));
     return pkg.name;
-}
+};
 
 /**
  * @method this._srcExists
@@ -86,9 +114,66 @@ ModuleGenerator.prototype._srcExists = function _srcExists() {
      var srcPathTab = process.cwd().split("/");
      var length = srcPathTab.length;
      return srcPathTab[length - 1] === "src";
-}
-
+};
 
 ModuleGenerator.prototype._generateMeta = function() {
-    
+    var meta = JSON.parse(this.engine(this.read('meta/_meta.json'), this));
+
+    if (this.configuration['i18n']) {
+        meta.lang = [
+            'en'
+        ];
+
+        if (!meta.requires || !meta.requires.length) {
+            meta.requires = ['intl'];
+        } else if (!!~meta.requires.indexOf('intl')) { //ensure "intl" not in deps
+            meta.requires.push('intl');
+        }
+    }
+
+    if (this.configuration['assets']) {
+        meta.skinnable = true;
+    }
+
+    if (this.configuration['templates']) {
+        if (!meta.requires || !meta.requires.length) {
+            meta.requires = ['handlebars-base'];
+        } else if (!!~meta.requires.indexOf('handlebars-base')) { //ensure "handlebars-base" not in deps
+            meta.requires.push('handlebars-base');
+        }
+    }
+
+    return JSON.stringify(meta);
+
+};
+
+
+ModuleGenerator.prototype._generateBuild = function() {
+    var build = JSON.parse(this.engine(this.read('_build.json'), this));
+
+    if (this.configuration['templates']) {
+
+        //template compilation
+        if (!build.exec || !build.exec.length) {
+            build.exec = ['yo yui3:handlebars'];
+        } else if (!!~build.execs.indexOf('yo yui3:handlebars')) { 
+            build.exec.push('yo yui3:handlebars');
+        }
+
+        //post clean
+        if (!build.postexec || !build.postexec.length) {
+            build.postexec = ['yo yui3:post_clean'];
+        } else if (!!~build.execs.indexOf('yo yui3:post_clean')) { 
+            build.postexec.push('yo yui3:post_clean');
+        }
+
+        try {
+            build.builds[this.name].jsfiles.unshift('../templates/'+this.name+'.js')
+        } catch (err) {
+            this.log.error('fail to add template to the build file, ensure the build.json file has been created with yo yui3:module');
+        }
+    }
+
+    return JSON.stringify(build);
+
 }
